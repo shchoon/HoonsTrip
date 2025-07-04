d2c6a4126a96d8696252e61eda85fa8cb2d888ac
shchoon <acckr0604@gmail.com>
2025-07-04 16:05:09 +0900

    style: flightCard layout 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Carousel from "./components/Carousel/Carousel";
+import FlightCard from "./components/Card/FlightCard";
 
 function App() {
+  const testData = {
+    id: 10,
+    airlineName: "Thai Airways",
+    airlineLogoUrl: "/images/thai-airways-logo.png",
+    departureAirport: "ICN",
+    arrivalAirport: "BKK",
+    arrivalCountry: "태국",
+    outboundDate: "2024-09-12",
+    inboundDate: "2024-09-17",
+    price: 310000,
+    currency: "KRW",
+    promotionTag: "특가",
+  };
+
   return (
     <>
       <Header />
       <Carousel />
+      <FlightCard flightData={testData} />
     </>
   );
 }
--- client/src/type.ts
+++ client/src/type.ts
@@ -3,3 +3,17 @@ export type Caoursel = {
   title: string;
   image: string;
 };
+
+export type Flight = {
+  id: number;
+  airlineName: string;
+  airlineLogoUrl: string;
+  departureAirport: string; // 출발 공항 코드
+  arrivalAirport: string; // 도착 공항 코드
+  arrivalCountry: string; // 도착 국가명
+  outboundDate: string; // 출국 날짜 (ISO 형식)
+  inboundDate: string; // 입국 날짜 (ISO 형식)
+  price: number; // 가격
+  currency: string; // 통화 단위
+  promotionTag: string; // 프로모션 태그 (ex: 특가, 한정)
+};
