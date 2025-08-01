27b30ab9c0383b6e1c8ad417e849eb0f5dce4369
shchoon <acckr0604@gmail.com>
2025-08-01 19:13:01 +0900

    playwright.config webserver 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./test/setup.ts",
+    exclude: ["tests/**"],
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ["text", "json-summary", "json"],
