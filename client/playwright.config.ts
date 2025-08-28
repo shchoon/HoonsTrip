5860e00bb6ca936ba17f397bc4c595f5cdea588e
shchoon <acckr0604@gmail.com>
2025-08-28 18:24:16 +0900

    playwright.config 수정

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: isCI ? "npm run build & npm run start" : "npm run dev",
+      command: isCI ? "npm run build && npm run start" : "npm run dev",
       url: "http://localhost:3000",
       name: "Frontend",
       reuseExistingServer: !process.env.CI,
